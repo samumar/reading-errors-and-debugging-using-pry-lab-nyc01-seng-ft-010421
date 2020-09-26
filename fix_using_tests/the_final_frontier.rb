d1632a3db921d20a435d51e19d0a92dfe113a7a0
samumar43 <samumar43@gmail.com>
2020-09-26 19:22:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,11 +1,7 @@
 # don't forget to add: require 'pry'
 
-def generate_star_date
-  (rand(100000) + 400000) / 10.0
-end
 
-
-def state_log(star_date)
+def engage(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
